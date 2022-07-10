d7509d779c64b6325949bc36b8c4ecc0d706feb1
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-10 20:18:01 +0530

    UC3: Invalid Mood

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/CustomMoodAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class CustomMoodAnalyserException: Exception
+    {
+        //Creating types of Exception may arise
+        public enum ExceptionType
+        {
+            Empty_Message, Null_Message
+        }
+        private readonly ExceptionType type;
+
+        public CustomMoodAnalyserException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
